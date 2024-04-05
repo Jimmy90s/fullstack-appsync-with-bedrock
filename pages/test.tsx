ad5d35ee2a093818948210ee67e3797f3a945e31
jimmy90s <jimmy90s@outlook.com>
2024-04-04 21:07:38 -0700

    changes from useEffect to getStaticProps

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,24 +1,31 @@
-import { CorePortfolio } from "@/backend/src/API";
+// import { CorePortfolio } from "@/backend/src/API";
 import { Navbar } from "@/components/navbar";
+import type { InferGetStaticPropsType, GetStaticProps } from "next";
 import { useState, useEffect } from "react";
 
-export default function Profile() {
-  const [data, setData] = useState<null | CorePortfolio[]>([]);
-  const [isLoading, setLoading] = useState(true);
+type CorePortfolio = {
+  __typename: "CorePortfolio";
+  date: string;
+  ticker: string;
+  return?: string | null;
+  weight?: string | null;
+  weighted_return?: string | null;
+  createdAt: string;
+  updatedAt: string;
+  owner?: string | null;
+};
 
-  useEffect(() => {
-    fetch("/api/date/route")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-    console.log(data);
-  }, [null]);
+export const getStaticProps = (async (context) => {
+  const res = await fetch("http://localhost:3000/api/date/route");
+  const data = await res.json();
+  return { props: { data } };
+}) satisfies GetStaticProps<{
+  data: CorePortfolio;
+}>;
 
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
-  console.log(data);
+export default function Page({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Navbar />
@@ -35,7 +42,7 @@ export default function Profile() {
             </tr>
           </thead>
           <tbody>
-            {data?.map((explanation) => (
+            {data?.map((explanation: any) => (
               <tr key={explanation.ticker} className="hover">
                 <td>{explanation.ticker}</td>
                 <td>{(Number(explanation.weight) * 100).toFixed(1)}%</td>
@@ -51,3 +58,52 @@ export default function Profile() {
     </>
   );
 }
+// export default function Profile() {
+// const [data, setData] = useState<null | CorePortfolio[]>([]);
+// const [isLoading, setLoading] = useState(true);
+
+// useEffect(() => {
+//   fetch("/api/date/route")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       setData(data);
+//       setLoading(false);
+//     });
+//   console.log(data);
+// }, [null]);
+
+// if (isLoading) return <p>Loading...</p>;
+// if (!data) return <p>No profile data</p>;
+// console.log(data);
+//   return (
+//     <>
+//       <Navbar />
+//       <h1 className=" m-3 text-center">{data[0].date.substring(0, 10)}</h1>
+
+//       <div className="hero min-h-screen m-3">
+//         <table className="table hero-content text-center">
+//           <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+//             <tr>
+//               <th>Ticker</th>
+//               <th>Weight</th>
+//               <th>Return</th>
+//               <th>Weighted Return</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {data?.map((explanation) => (
+//               <tr key={explanation.ticker} className="hover">
+//                 <td>{explanation.ticker}</td>
+//                 <td>{(Number(explanation.weight) * 100).toFixed(1)}%</td>
+//                 <td>{(Number(explanation.return) * 100).toFixed(1)}%</td>
+//                 <td>
+//                   {(Number(explanation.weighted_return) * 100).toFixed(1)}%
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </>
+//   );
+// }
