101392ae05ea3f9d17ca1862d87b38e1b03b0598
jimmy90s <jimmy90s@outlook.com>
2024-04-07 13:48:47 -0700

    changed fetch method to getStaticProps

--- pages/test.tsx
+++ pages/test.tsx
@@ -16,66 +16,17 @@ type CorePortfolio = {
   owner?: string | null;
 };
 
-// export const getStaticProps = (async (context) => {
-//   const res = await fetch("http://localhost:3000/api/date/route");
-//   const data = await res.json();
-//   return { props: { data } };
-// }) satisfies GetStaticProps<{
-//   data: CorePortfolio;
-// }>;
+export const getStaticProps = (async (context) => {
+  const res = await fetch("http://localhost:3000/api/date/route");
+  const data = await res.json();
+  return { props: { data } };
+}) satisfies GetStaticProps<{
+  data: CorePortfolio;
+}>;
 
-// export default function Page({
-//   data,
-// }: InferGetStaticPropsType<typeof getStaticProps>) {
-//   return (
-//     <>
-//       <Navbar />
-//       <h1 className=" m-3 text-center">{data[0].date.substring(0, 10)}</h1>
-
-//       <div className="hero min-h-screen m-3">
-//         <table className="table hero-content text-center">
-//           <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
-//             <tr>
-//               <th>Ticker</th>
-//               <th>Weight</th>
-//               <th>Return</th>
-//               <th>Weighted Return</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             {data?.map((explanation: any) => (
-//               <tr key={explanation.ticker} className="hover">
-//                 <td>{explanation.ticker}</td>
-//                 <td>{(Number(explanation.weight) * 100).toFixed(1)}%</td>
-//                 <td>{(Number(explanation.return) * 100).toFixed(1)}%</td>
-//                 <td>
-//                   {(Number(explanation.weighted_return) * 100).toFixed(1)}%
-//                 </td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-//     </>
-//   );
-// }
-export default function Profile() {
-  const [data, setData] = useState<null | CorePortfolio[]>([]);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("/api/date/route")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-    console.log(data);
-  }, []);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
-  console.log(data);
+export default function Page({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Navbar />
@@ -92,7 +43,7 @@ export default function Profile() {
             </tr>
           </thead>
           <tbody>
-            {data?.map((explanation) => (
+            {data?.map((explanation: any) => (
               <tr key={explanation.ticker} className="hover">
                 <td>{explanation.ticker}</td>
                 <td>{(Number(explanation.weight) * 100).toFixed(0)}%</td>
@@ -108,3 +59,52 @@ export default function Profile() {
     </>
   );
 }
+// export default async function Profile() {
+//   const [data, setData] = useState<null | CorePortfolio[]>([]);
+//   const [isLoading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     fetch("/api/date/route")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         setData(data);
+//         setLoading(false);
+//       });
+//     console.log(data);
+//   }, []);
+
+//   if (isLoading) return <p>Loading...</p>;
+//   if (!data) return <p>No profile data</p>;
+//   console.log(data);
+//   return (
+//     <>
+//       <Navbar />
+//       <h1 className=" m-3 text-center">{data[0].date.substring(0, 10)}</h1>
+
+//       <div className="hero min-h-screen m-3">
+//         <table className="table hero-content text-center">
+//           <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+//             <tr>
+//               <th>Ticker</th>
+//               <th>Weight</th>
+//               <th>Return</th>
+//               <th>Weighted Return</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {data?.map((explanation) => (
+//               <tr key={explanation.ticker} className="hover">
+//                 <td>{explanation.ticker}</td>
+//                 <td>{(Number(explanation.weight) * 100).toFixed(0)}%</td>
+//                 <td>{(Number(explanation.return) * 100).toFixed(2)}%</td>
+//                 <td>
+//                   {(Number(explanation.weighted_return) * 100).toFixed(2)}%
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </>
+//   );
+// }
